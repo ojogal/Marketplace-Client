747da0a5a04fa0ed745240d4583b9272b0bad2a7
ojogal <ojog.al@gmail.com>
2023-01-12 18:57:14 +0200

    BrowserRouter added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from './components/AppRouter.js'
 
 const App = () => {
   return (
-    <div>
-      Det fungerer
-    </div>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   );
 }
 
