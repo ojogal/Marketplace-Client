3d0ebbcd83596406ee5410bd0834de55198399e6
ojogal <ojog.al@gmail.com>
2023-01-13 21:59:31 +0200

    Navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,11 @@ const Navbar = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="#home">Kjøpe en enhet</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#home">Hjem</Nav.Link>
+          <Nav.Link href="#features">Egenskaper</Nav.Link>
+          <Nav.Link href="#pricing">Prisseting</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
