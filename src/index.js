0d845064896e4551782bfe3b6f432e4d5c080153
ojogal <ojog.al@gmail.com>
2023-01-13 21:45:49 +0200

    Device value added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import UserStore from './store/userStore';
 import { Context } from '../index.js'
+import DeviceStore from './store/DeviceStore';
 
 export const Context = createContext(null);
 
 ReactDOM.render(
     <Context.Provider value={{
         user: UserStore(),
+        device: DeviceStore()
     }}>
         <App/>
     </Context.Provider>,
