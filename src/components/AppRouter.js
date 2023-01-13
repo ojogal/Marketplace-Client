c88a3166b909fbc9912bdaa5d67ff88b509c9a58
ojogal <ojog.al@gmail.com>
2023-01-13 13:28:53 +0200

    Routes updated

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -2,17 +2,19 @@ import React from "react";
 import { Switch, Route, Redirect } from 'react';
 import { authRoutes } from "../routes";
 import { publicRoutes } from "../routes";
+import { SHOP_ROUTE } from "../utils/consts";
 
 const AppRouter = () => {
-  const isAuth = false;
+  const isAuth = true;
   return (
     <Switch>
       { isAuth && authRoutes.map(({ path, Component }) => {
-        <Route key={ path } path={ path } component={ Component } exact />
+        return <Route key={ path } path={ path } component={ Component } exact />
       }) };
       { publicRoutes.map(({ path, Component }) => {
-        <Route key={ path } path={ path } component={ Component } exact />
-      }) }
+        return <Route key={ path } path={ path } component={ Component } exact />
+      }) };
+      <Redirect to={ SHOP_ROUTE } />
     </Switch>
   )
 };
