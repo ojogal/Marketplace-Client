f6cf51d0c251987fa546a0edf736d9dabf07d54f
ojogal <ojog.al@gmail.com>
2023-01-13 10:39:09 +0200

    Routes added

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { authRoutes } from "../routes";
+import { publicRoutes } from "../routes";
 
 const AppRouter = () => {
+  const isAuth = false;
   return (
-    <div>
-      
-    </div>
+    <Switch>
+      { isAuth && authRoutes.map(({ path, Component }) => {
+        <Route key={ path } path={ path } component={ Component } exact />
+      }) };
+      { publicRoutes.map(({ path, Component }) => {
+        <Route key={ path } path={ path } component={ Component } exact />
+      }) }
+    </Switch>
   )
 }
